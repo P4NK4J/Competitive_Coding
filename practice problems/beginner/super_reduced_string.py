d9a68cde1f68525c93033f7ea503a6293dfbd893
pankaj kandpal <35001775+P4NK4J@users.noreply.github.com>
2019-07-08 07:06:18 -0700

    Add files via upload

---
+++ practice problems/beginner/super_reduced_string.py
@@ -0,0 +1,17 @@
+s = input()
+l = len(s)
+s = list(s)
+while True:
+    flag = 0
+    j = 0
+    while j < len(s)-1:
+        if s[j] == s[j+1]:
+            s.pop(j)
+            s.pop(j)
+            flag = 1
+        j += 1
+    if flag == 0:
+        break
+
+if len(s) == 0: print('Empty String')
+else: print(''.join(s))
